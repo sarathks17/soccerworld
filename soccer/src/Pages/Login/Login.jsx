9d7a47af9cbb4c7f852323f2f19caa6afbc7e28c
sarathkumar s <sarathkumar93622@gmail.com>
2024-12-13 14:23:25 +0530

    login section completed working on checkbox

--- soccer/src/Pages/Login/Login.jsx
+++ soccer/src/Pages/Login/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
-
+import './Login.css'
+import Footer from '../../Components/Footer/Footer'
 const Login = () => {
   return (
-    <div>
-      LOGIN PAGE
+    <>
+   <h6>LOGIN</h6>
+   <hr />
+    <div className='login-container'>
+      <div className="login-components">
+        <div className='components'>
+        <label >Name:</label>
+        <input type="text" />
+        </div>
+        <div className='components'>
+        <label >Email:</label>
+        <input type="email" />
+        </div>
+        <div className='components'>
+       <label >Age:</label>
+       <input type="number" />
+       </div>
+       <div className='components'>
+      <label >Number:</label>
+      <input type="number" />
+      </div>
+      <div className='components'>
+        <label >Password:</label>
+        <input type="password" />
+       
+        </div>
+      <div className='components'>
+        <label >Confirm password:</label>
+        <input type="password" />
+
+      
+        </div>
+     
+       
+      </div>
+      <div className='forsubmit'>
+      <button>LOGIN</button>
+      </div>
     </div>
+    <hr />
+    <Footer/>
+    </>
   )
 }
 
