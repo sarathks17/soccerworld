14015e6c65efc20a4cf056d9ce1534819e2880e8
sarathkumar s <sarathkumar93622@gmail.com>
2024-12-20 15:45:23 +0530

    bcrypt issue solved

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,6 @@
 import userModel from "../Models/userModel.js";
 import jwt from 'jsonwebtoken';
-import bycrypt from "bcrypt"
+import bcrypt from 'bcryptjs';
 import validator from "validator"
 
 // login user
@@ -13,7 +13,7 @@ const loginUser = async(req,res)=> {
         if(!user){
             return res.json({success:false,message:"User does not exist"})
         }
-        const isMatch = await bycrypt.compare(password,user.password)
+        const isMatch = await bcrypt.compare(password,user.password)
 
         if(!isMatch){
             return res.json({success:false,message:"Invalid credentials"})
@@ -42,8 +42,8 @@ const registerUser = async(req,res)=> {
     if(password.length < 8){
         return res.json({success:false,message:"Please enter strong password"})
     }
-    const salt = await bycrypt.genSalt(10)
-    const hashedPassword = await bycrypt.hash(password,salt)
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
 
     const newUser = new userModel({
         name:name,
